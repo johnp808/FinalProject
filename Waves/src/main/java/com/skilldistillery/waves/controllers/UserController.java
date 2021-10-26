48aa8bf71d680504789a6ead2039af08990a5654
Kbow009 <bowman.kristin@gmail.com>
2021-10-26 15:02:33 -0700

    angular

--- Waves/src/main/java/com/skilldistillery/waves/controllers/UserController.java
+++ Waves/src/main/java/com/skilldistillery/waves/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.waves.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import com.skilldistillery.waves.services.UserService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin({"*", "http://localhost:9090"})
 public class UserController {
 
 	@Autowired
