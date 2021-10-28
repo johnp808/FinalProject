db76e0830f82018598e399986889a74a48606063
Yanyan07 <yenj2015@gmail.com>
2021-10-28 15:20:08 -0600

    update controller

--- Waves/src/main/java/com/skilldistillery/waves/controllers/WeatherController.java
+++ Waves/src/main/java/com/skilldistillery/waves/controllers/WeatherController.java
@@ -6,8 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.waves.entities.Report;
 import com.skilldistillery.waves.entities.Weather;
 import com.skilldistillery.waves.services.WeatherService;
 
@@ -29,5 +35,38 @@ public class WeatherController {
 			res.setStatus(404);
 		}
 		return weatherList;
+	}
+	
+	@PutMapping("weather/{weatherId}/{beachId}")
+	public Weather updateWeatherByIdAndUsername(
+			@PathVariable int weatherId,
+			@PathVariable int beachId,
+			@RequestBody Weather weather,
+			HttpServletResponse res,
+			Principal principal
+			) {
+		Weather updated = weatherSvc.updateWeatherById(weatherId, beachId, principal.getName(), weather);
+		if(updated == null) {
+			res.setStatus(404);
+		}
+		return updated;
+	}
+	
+
+	@PostMapping("weather/{beachId}")
+	public Weather createWeatherByUsername(
+			@PathVariable int beachId,
+			@RequestBody Weather weather,
+			HttpServletResponse res,
+			Principal principal
+			) {
+		Weather newWeather = weatherSvc.AddWeather(beachId, principal.getName(), weather);
+		if(newWeather != null) {
+			res.setStatus(201);
+			return newWeather;
+		}else {
+			res.setStatus(404);
+			return null;
+		}
 	}
 }
--- Waves/src/main/java/com/skilldistillery/waves/services/WeatherServiceImpl.java
+++ Waves/src/main/java/com/skilldistillery/waves/services/WeatherServiceImpl.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.waves.services;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.skilldistillery.waves.entities.Beach;
+import com.skilldistillery.waves.entities.Report;
+import com.skilldistillery.waves.entities.User;
 import com.skilldistillery.waves.entities.Weather;
+import com.skilldistillery.waves.repositories.BeachRepository;
+import com.skilldistillery.waves.repositories.UserRepository;
 import com.skilldistillery.waves.repositories.WeatherRepository;
 
 @Service
@@ -11,10 +17,43 @@ public class WeatherServiceImpl implements WeatherService {
 	
 	@Autowired
 	private WeatherRepository weatherRepo;
+	@Autowired
+	private BeachRepository beachRepo;
+	@Autowired
+	private UserRepository userRepo;
 
 	@Override
 	public List<Weather> getWeatherListByUsername(String username) {
 		return weatherRepo.findByUser_Username(username);
 	}
+	
+	@Override
+	public Weather updateWeatherById(int weatherId, int beachId, String username, Weather weather) {
+		Optional<Weather> managedWeather = weatherRepo.findById(weatherId);
+		Beach beach = beachRepo.findById(beachId);
+		User user = userRepo.findByUsername(username);
+		if(managedWeather.isPresent() && user!=null && beach!=null && weather!=null) {
+			weather.setId(weatherId);
+			weather.setBeach(beach);
+			weather.setUser(user);
+			weatherRepo.saveAndFlush(weather);
+			return weather;
+		}
+		return null;
+	}
+	
+	@Override
+	public Weather AddWeather(int beachId, String username, Weather weather) {
+		Beach beach = beachRepo.findById(beachId);
+		User user = userRepo.findByUsername(username);
+		if(user!=null && beach!=null && weather!=null) {
+			weather.setBeach(beach);
+			weather.setUser(user);
+			weatherRepo.saveAndFlush(weather);
+			return weather;
+		}
+		return null;
+	}
+	
 
 }
