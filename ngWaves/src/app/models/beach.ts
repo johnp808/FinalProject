4022d9fd79c3d1f77d1d7018b1074b0cb1e4e5a2
Cindy <cindy@Students-MacBook-Pro.local>
2021-10-28 21:03:57 -0600

    created beach, location, user models

--- ngWaves/src/app/models/beach.ts
+++ ngWaves/src/app/models/beach.ts
@@ -1,12 +1,44 @@
 export class Beach {
   id: number;
-
+  name: String;
+  description: String;
+  profileImage: String;
+  locationId: number;
+  operational: boolean;
+  petFriendly: String;
+  parkingAvailable: String;
+  priceOfAdmissions: String;
+  hours: String;
+  enabled: boolean;
+  rating: number;
 
   constructor(
     id: number = 0,
+    name: String = '',
+    description: String = '',
+    profileImage: String = '',
+    locationId: number = 0,
+    operational: boolean = true,
+    petFriendly: String = '',
+    parkingAvailable: String = '',
+    priceOfAdmissions: String = '',
+    hours: String = '',
+    enabled: boolean = true,
+    rating: number = 0
 
   ){
     this.id = id;
+    this.name = name;
+    this.description = description;
+    this.profileImage = profileImage;
+    this.locationId = locationId;
+    this.operational = operational;
+    this.petFriendly = petFriendly;
+    this.parkingAvailable = parkingAvailable;
+    this.priceOfAdmissions = priceOfAdmissions;
+    this.hours = hours;
+    this.enabled = enabled;
+    this.rating = rating;
 
   }
 }
