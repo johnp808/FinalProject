67d4f0661e90108d1a95a0b2a95cbc0cdbc9fa6d
Cindy <cindy@Students-MacBook-Pro.local>
2021-11-01 09:01:13 -0600

    changes user passwords to mirror usernames

--- ngWaves/src/app/components/home/show-beach/show-beach.component.ts
+++ ngWaves/src/app/components/home/show-beach/show-beach.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Beach } from 'src/app/models/beach';
+import { Report } from 'src/app/models/report';
 import { BeachService } from 'src/app/services/beach.service';
+import { ReportService } from 'src/app/services/report.service';
 
 @Component({
   selector: 'app-show-beach',
@@ -16,16 +18,18 @@ export class ShowBeachComponent implements OnInit {
   title = 'ngBeach';
 
   beaches: Beach[] = [];
+  reports: Report[] = [];
 
   selected: Beach | null = null;
   newBeach: Beach | null = new Beach();
   editBeach: Beach | null = null;
+  report: Report | null = null;
 
-  constructor(private beachService: BeachService) { }
+  constructor(private beachService: BeachService,
+    private reportService: ReportService)
+    {
 
-  displayBeach(beach: Beach): void {
-    this.selected = beach;
-  }
+    }
 
   reloadBeaches(): void {
     this.beachService.index().subscribe(
@@ -38,4 +42,14 @@ export class ShowBeachComponent implements OnInit {
       }
     );
   }
+
+  displayBeach(beach: Beach): void {
+    this.selected = beach;
+  }
+
+  // displayReport(report:Report): Report beachReport {
+  //   this.report = beachReport
+  // }
+
+
 }
