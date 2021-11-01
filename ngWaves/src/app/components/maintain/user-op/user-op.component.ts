1f79b905be5b4c3fdfb654122e4f6ce9f79eedad
Yanyan07 <yenj2015@gmail.com>
2021-11-01 09:34:37 -0600

    update admin

--- ngWaves/src/app/components/maintain/user-op/user-op.component.ts
+++ ngWaves/src/app/components/maintain/user-op/user-op.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user-op',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-op.component.css']
 })
 export class UserOpComponent implements OnInit {
+  isLogin: boolean = false;
+  role: string = '';
+  userList: User[] = [];
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.isLogin = this.authService.checkLogin();
+    if(this.isLogin){
+      this.role = this.authService.currUser.role;
+    }
+  }
+
+  reloadUsers(): void{
+    this.authService.index().subscribe(
+      users => {
+        this.userList = users;
+        console.log(this.userList);
+      },
+      fail => {
+        console.error('UserListComponent.reloadUsers(): error getting user list');
+        console.log(fail);
+      }
+    );
+  }
+
+  reverse(user: User){
+    this.authService.reverse(user).subscribe(
+      users => {
+        this.reloadUsers();
+      },
+      fail => {
+        console.error('UserListComponent.reverse(): error reversing enabled');
+        console.log(fail);
+      }
+    );
   }
 
 }
